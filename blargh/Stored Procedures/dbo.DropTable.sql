f4d1ee59aa7d8c017db5e7e0d3efda5013f0b93f
Chris Harmon <charmon@xceligent.com>
2016-02-02 13:20:39 -0600

    Breaking something intentionally so I can try to revert it later.

--- blargh/Stored Procedures/dbo.DropTable.sql
+++ blargh/Stored Procedures/dbo.DropTable.sql
@@ -4,16 +4,22 @@ GO
 SET ANSI_NULLS ON
 GO
 
+
 CREATE PROCEDURE [dbo].[DropTable]
 AS
 BEGIN
-    IF object_id('dbo.DropMe','U') IS NOT NULL
+    IF OBJECT_ID('dbo.DropMe','U') IS NOT NULL
     TRUNCATE TABLE dbo.DropMe;
     
     PRINT 'I dropped a table. Oops!';
+
+    --Whoops, now I introduced a bug too! Man, I suck at SQL development.
+    SELECT 1/0
 END
 
+
 GO
+
 
 GRANT EXECUTE ON  [dbo].[DropTable] TO [TestUser]
 GO
