4995fb864d6956e348b36a1054f26ab62771eb63
Chris Harmon <charmon@xceligent.com>
2016-02-02 13:29:39 -0600

    Fixed the bug I introduced in the last commit using the version from the commit before it.

--- blargh/Stored Procedures/dbo.DropTable.sql
+++ blargh/Stored Procedures/dbo.DropTable.sql
@@ -4,21 +4,17 @@ GO
 SET ANSI_NULLS ON
 GO
 
-
 CREATE PROCEDURE [dbo].[DropTable]
 AS
 BEGIN
-    IF OBJECT_ID('dbo.DropMe','U') IS NOT NULL
+    IF object_id('dbo.DropMe','U') IS NOT NULL
     TRUNCATE TABLE dbo.DropMe;
     
     PRINT 'I dropped a table. Oops!';
-
-    --Whoops, now I introduced a bug too! Man, I suck at SQL development.
-    SELECT 1/0
 END
 
-
 GO
+
 
 
 GRANT EXECUTE ON  [dbo].[DropTable] TO [TestUser]
